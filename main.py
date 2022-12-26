5efb6c20c3ed96836a992a7ec0a73b9daccf0d38
JohnKlein985 <John@apexinnovations.com>
2022-12-26 09:32:17 -0600

    Added I2C display. Moved all libraries to libs folder.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import tm1637
+from machine import Pin, ADC, I2C
+from libs.tm1637 import *
+from libs.ssd1306 import *
+from libs.PicoDHT22 import *
 from classes.button import button
 from classes.led import led
 from classes.buzzer import buzzer
-from machine import Pin, ADC
-from PicoDHT22 import PicoDHT22
 from projects.TransitorTemp import TransitorTemp
 from projects.DHTTemp import DHTTemp
 from projects.SocketTemperature import SocketTemperature
 
+#SETUP - switches, buzzers, leds, displays, sensors, etc...
 board_led = led(Pin("LED", Pin.OUT))
-board_buzzer = buzzer(Pin(1, Pin.OUT))
+board_buzzer = buzzer(Pin(10, Pin.OUT))
 btn1 = button(Pin(2, Pin.IN, Pin.PULL_UP)) #Project A - Temperature Display - Transitor
 btn2 = button(Pin(3, Pin.IN, Pin.PULL_UP)) #Project B - Temperature Display - DHT11
 btn3 = button(Pin(4, Pin.IN, Pin.PULL_UP)) #Project C - Socket Temperature Display - DHT11
@@ -18,13 +20,15 @@ led1 = led(Pin(6, Pin.OUT))
 led2 = led(Pin(7, Pin.OUT))    
 led3 = led(Pin(8, Pin.OUT))    
 led4 = led(Pin(9, Pin.OUT))
-
-digital_display = tm1637.TM1637(clk=Pin(17), dio=Pin(16))
+digital_display = TM1637(clk=Pin(17), dio=Pin(16))
 transistor_sensor = ADC(26)
 dht_sensor=PicoDHT22(Pin(28,Pin.IN,Pin.PULL_UP),dht11=True)
-
+i2cdisplay = SSD1306_I2C(128, 64, I2C(0, sda=Pin(0), scl=Pin(1), freq=400000))
 
 #Main.py has started, show board LED and display
+i2cdisplay.fill(0)
+i2cdisplay.text('Hello, World!', 0, 0, 1)
+i2cdisplay.show()
 digital_display.scroll('ready')
 board_led.on()
 print('READY!')
@@ -61,5 +65,3 @@ while True:
         led2.off()
         print('Exiting project C...')
         digital_display.scroll('ready')
-
-
