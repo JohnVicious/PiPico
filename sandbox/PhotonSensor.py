fcafe6c598e45b2c4861ce8a76f832fc6e7f9415
JohnKlein985 <John@apexinnovations.com>
2023-01-04 19:28:34 -0600

    Reed switch

--- sandbox/PhotonSensor.py
+++ sandbox/PhotonSensor.py
@@ -6,9 +6,13 @@ from classes.button import button
 from classes.led import led
 from classes.buzzer import buzzer
 import time
+import random
+
 #SETUP - switches, buzzers, leds, displays, sensors, etc...
 board_led = led(Pin("LED", Pin.OUT))
 i2cdisplay = SSD1306_I2C(128, 64, I2C(0, sda=Pin(0), scl=Pin(1), freq=400000))
+photo_sensor = ADC(26)
+
 
 #Show startup
 board_led.off()
@@ -18,7 +22,19 @@ i2cdisplay.show()
 time.sleep_ms(1000)
 
 while True:
-    board_led.toggle()
+    photons = photo_sensor.read_u16()
+
+    if photons > 10000:
+        lightStatus = 'on'
+        board_led.on()
+    else:
+        lightStatus = 'off'
+        board_led.off()
+
+
+    i2cdisplay.fill(0)
+    i2cdisplay.text('Light is ' + str(lightStatus), 0, 30, 1)
+    i2cdisplay.show()
 
     time.sleep_ms(1000)
     
--- sandbox/ReedSwitch.py
+++ sandbox/ReedSwitch.py
@@ -6,9 +6,13 @@ from classes.button import button
 from classes.led import led
 from classes.buzzer import buzzer
 import time
+
 #SETUP - switches, buzzers, leds, displays, sensors, etc...
 board_led = led(Pin("LED", Pin.OUT))
+sensor_led = led(Pin(13, Pin.OUT))
+reed_sensor = Pin(5, Pin.IN, Pin.PULL_UP)
 i2cdisplay = SSD1306_I2C(128, 64, I2C(0, sda=Pin(0), scl=Pin(1), freq=400000))
+
 
 #Show startup
 board_led.off()
@@ -18,7 +22,14 @@ i2cdisplay.show()
 time.sleep_ms(1000)
 
 while True:
-    board_led.toggle()
 
-    time.sleep_ms(1000)
-    
+    board_led.toggle()
+    sensor_led.toggle()
+    i2cdisplay.fill(0)
+    i2cdisplay.text('Reed switch: ', 0, 10, 1)
+
+    if reed_sensor.value() == 0:
+        i2cdisplay.text('detected!', 0, 30, 1)
+
+    i2cdisplay.show()
+    time.sleep_ms(100)
